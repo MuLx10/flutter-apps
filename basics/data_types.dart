61eb07a37b19c6f51883d75715b0a200a93278bc
mulx10 <mehulkumarnirala@iitkgp.ac.in>
2020-04-21 18:53:30 +0530

    Added Basic: DataTypes

---
+++ basics/data_types.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Basic data types/
+  int age=18;
+  double value=18.0;
+  String name="James Bond";
+  bool isFollowing=true;
+  
+  // Declaring a list with values
+  var ageList=[18,23,67,34];
+  
+  // Declaring a list with fixed size and all values null
+  var x= List(3);
+}
